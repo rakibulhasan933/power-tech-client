95a988a52ae99110c18ea3d1eca2cf7e809a4856
RAKIBUL <rakibulmd933@gmail.com>
2023-05-12 01:29:16 +0600

    [ADD] card

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
 import Banner from '@/components/Banner/page'
-import Navbar from '@/components/Navbar/page'
 import Products from '@/components/Products/page'
 import Steps from '@/components/Steps/page'
 import WeekDeal from '@/components/Weekdeal/page'
-import Image from 'next/image'
 
 export default function Home() {
   return (
