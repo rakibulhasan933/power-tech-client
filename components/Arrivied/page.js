1adcb870b6f6f894df78c3bda3501b3a20878a27
RAKIBUL <rakibulmd933@gmail.com>
2023-05-14 00:40:45 +0600

    [ADD] card

--- components/Arrivied/page.js
+++ components/Arrivied/page.js
@@ -18,68 +18,68 @@ function Arrived() {
 				</div>
 			</div>
 			<div className="grid grid-cols-2 gap-6 pt-20">
-				<div className="flex flex-row pt-5 bg-blue-100 rounded px-7">
-					<div className="basis-1/2">
-						<Image src={mac} className="w-full rounded transition-opacity opacity-0 duration-[1s] mb-8"
+				<div className="flex flex-row p-4 bg-blue-100 rounded">
+					<div className="flex items-center justify-center p-2 basis-1/2">
+						<Image src={mac} className="w-full h-full rounded transition-opacity opacity-0 duration-[1s]"
 							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='laptop' />
 					</div>
-					<div className="flex flex-col py-2 ml-6 basis-1/2">
-						<p className="mb-1 text-xs font-light">Available now</p>
-						<h2 className="mb-1 text-xl font-bold ">the new <br />
+					<div className="flex flex-col content-center p-4 basis-1/2">
+						<p className="my-2 text-xs font-light ">Available now</p>
+						<h2 className="my-2 text-xl font-bold ">the new <br />
 							POWERTECH laptop</h2>
-						<p className="mb-1 text-xs font-light">See rich, vibrant colors and incredible
+						<p className="my-2 text-xs font-light">See rich, vibrant colors and incredible
 							contrast, even in well-lit rooms.
 						</p>
-						<h2 className="mb-1 text-xl font-bold text-orange-500">$400</h2>
-						<button className='w-2/3 px-4 py-2 text-sm font-bold text-white bg-blue-400 rounded '>Add to Cart</button>
+						<h2 className="my-2 text-xl font-bold text-orange-500">$400</h2>
+						<button className='w-full px-8 py-4 text-sm font-bold text-white uppercase bg-blue-400 rounded '>Add to Cart</button>
 					</div>
 				</div>
-				<div className="flex flex-row pt-5 bg-blue-100 rounded px-7">
-					<div className="basis-1/2">
-						<Image src={mac} className="w-full rounded transition-opacity opacity-0 duration-[1s] mb-8"
+				<div className="flex flex-row p-4 bg-blue-100 rounded">
+					<div className="flex items-center justify-center p-2 basis-1/2">
+						<Image src={mac} className="w-full h-full rounded transition-opacity opacity-0 duration-[1s]"
 							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='laptop' />
 					</div>
-					<div className="flex flex-col py-2 ml-6 basis-1/2">
-						<p className="mb-1 text-xs font-light">Available now</p>
-						<h2 className="mb-1 text-xl font-bold ">the new <br />
+					<div className="flex flex-col content-center p-4 basis-1/2">
+						<p className="my-2 text-xs font-light ">Available now</p>
+						<h2 className="my-2 text-xl font-bold ">the new <br />
 							POWERTECH laptop</h2>
-						<p className="mb-1 text-xs font-light">See rich, vibrant colors and incredible
+						<p className="my-2 text-xs font-light">See rich, vibrant colors and incredible
 							contrast, even in well-lit rooms.
 						</p>
-						<h2 className="mb-1 text-xl font-bold text-orange-500">$400</h2>
-						<button className='w-2/3 px-4 py-2 text-sm font-bold text-white bg-blue-400 rounded '>Add to Cart</button>
+						<h2 className="my-2 text-xl font-bold text-orange-500">$400</h2>
+						<button className='w-full px-8 py-4 text-sm font-bold text-white uppercase bg-blue-400 rounded '>Add to Cart</button>
 					</div>
 				</div>
-				<div className="flex flex-row pt-5 bg-blue-100 rounded px-7">
-					<div className="basis-1/2">
-						<Image src={mac} className="w-full rounded transition-opacity opacity-0 duration-[1s] mb-8"
+				<div className="flex flex-row p-4 bg-blue-100 rounded">
+					<div className="flex items-center justify-center p-2 basis-1/2">
+						<Image src={mac} className="w-full h-full rounded transition-opacity opacity-0 duration-[1s]"
 							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='laptop' />
 					</div>
-					<div className="flex flex-col py-2 ml-6 basis-1/2">
-						<p className="mb-1 text-xs font-light">Available now</p>
-						<h2 className="mb-1 text-xl font-bold ">the new <br />
+					<div className="flex flex-col content-center p-4 basis-1/2">
+						<p className="my-2 text-xs font-light ">Available now</p>
+						<h2 className="my-2 text-xl font-bold ">the new <br />
 							POWERTECH laptop</h2>
-						<p className="mb-1 text-xs font-light">See rich, vibrant colors and incredible
+						<p className="my-2 text-xs font-light">See rich, vibrant colors and incredible
 							contrast, even in well-lit rooms.
 						</p>
-						<h2 className="mb-1 text-xl font-bold text-orange-500">$400</h2>
-						<button className='w-2/3 px-4 py-2 text-sm font-bold text-white bg-blue-400 rounded '>Add to Cart</button>
+						<h2 className="my-2 text-xl font-bold text-orange-500">$400</h2>
+						<button className='w-full px-8 py-4 text-sm font-bold text-white uppercase bg-blue-400 rounded '>Add to Cart</button>
 					</div>
 				</div>
-				<div className="flex flex-row pt-5 bg-blue-100 rounded px-7">
-					<div className="basis-1/2">
-						<Image src={mac} className="w-full rounded transition-opacity opacity-0 duration-[1s] mb-8"
+				<div className="flex flex-row p-4 bg-blue-100 rounded">
+					<div className="flex items-center justify-center p-2 basis-1/2">
+						<Image src={mac} className="w-full h-full rounded transition-opacity opacity-0 duration-[1s]"
 							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='laptop' />
 					</div>
-					<div className="flex flex-col py-2 ml-6 basis-1/2">
-						<p className="mb-1 text-xs font-light">Available now</p>
-						<h2 className="mb-1 text-xl font-bold ">the new <br />
+					<div className="flex flex-col content-center p-4 basis-1/2">
+						<p className="my-2 text-xs font-light ">Available now</p>
+						<h2 className="my-2 text-xl font-bold ">the new <br />
 							POWERTECH laptop</h2>
-						<p className="mb-1 text-xs font-light">See rich, vibrant colors and incredible
+						<p className="my-2 text-xs font-light">See rich, vibrant colors and incredible
 							contrast, even in well-lit rooms.
 						</p>
-						<h2 className="mb-1 text-xl font-bold text-orange-500">$400</h2>
-						<button className='w-2/3 px-4 py-2 text-sm font-bold text-white bg-blue-400 rounded '>Add to Cart</button>
+						<h2 className="my-2 text-xl font-bold text-orange-500">$400</h2>
+						<button className='w-full px-8 py-4 text-sm font-bold text-white uppercase bg-blue-400 rounded '>Add to Cart</button>
 					</div>
 				</div>
 			</div>
