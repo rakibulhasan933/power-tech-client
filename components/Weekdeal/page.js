2673ce49699dbcf30daec0e6a492083b935725d0
RAKIBUL <rakibulmd933@gmail.com>
2023-05-14 14:41:57 +0600

    [ADD] card

--- components/Weekdeal/page.js
+++ components/Weekdeal/page.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import React from 'react'
 import { AiOutlineArrowRight } from "react-icons/ai";
 import monitor from "../assets/monitor.jpg";
+
 function WeekDeal() {
 	return (
 		<div className="pt-10">
@@ -21,7 +22,7 @@ function WeekDeal() {
 					<Image src={monitor} className="w-full transition-opacity rounded opacity-0 duration-[1s] mb-6"
 						onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='monitor mac' />
 				</div>
-				<div className="flex flex-col justify-center pl-12 basis-1/2">
+				<div className="flex flex-col content-center justify-center pl-12 basis-1/2">
 					<p className='mb-6 text-base font-normal '>Available now</p>
 					<h3 className='mb-6 text-4xl font-bold '>Introducing the new <br /> POWERTECH LED</h3>
 					<p className="text-base font-normal mb-9">
@@ -31,7 +32,7 @@ function WeekDeal() {
 					<h3 className="mb-6 text-4xl font-extrabold text-orange-600 ">
 						$2,731
 					</h3>
-					<button className='w-1/4 px-2 py-1 text-white bg-blue-400 rounded '>Shop Now</button>
+					<button className='w-1/2 px-6 py-3 font-bold text-white bg-blue-400 rounded '>Shop Now</button>
 				</div>
 			</div>
 		</div>
