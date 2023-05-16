b42ce30cc1bfdbd5b4900315337f3d68c09e0d87
RAKIBUL <rakibulmd933@gmail.com>
2023-05-16 22:39:37 +0600

    [ADD] monitor

--- components/HighlightTask/page.js
+++ components/HighlightTask/page.js
@@ -6,41 +6,42 @@ import monitor from "../assets/monitor-highlight.jpg";
 
 function HighlightTask() {
 	return (
-		<div className="mx-40 py-36">
-			<section className="">
-				<div className="flex flex-row justify-between mb-10">
-					<div className="flex items-center ">
-						<div className="w-3 mr-4 bg-blue-400 h-11">
-						</div>
-						<h2 className="text-5xl font-bold ">Monitor</h2>
+		<section
+			className="mx-40 py-36">
+			<div className="flex flex-row justify-between mb-10">
+				<div className="flex items-center ">
+					<div className="w-3 mr-4 bg-blue-400 h-11">
 					</div>
-					<div className="flex flex-row items-center">
-						<button className=''>more</button>
-						<AiOutlineArrowRight className='w-full ' />
-					</div>
+					<h2 className="text-5xl font-bold ">Monitor</h2>
 				</div>
-				<div className="p-4 bg-blue-100 rounded-lg">
-					<div className="flex flex-col gap-4 rounded-md">
-						<div className="flex content-center p-12 rounded-2xl">
-							<Image src={monitor} className=" w-full  transition-opacity opacity-0   duration-[1s] px-4 mb-4"
-								onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='monitor' />
-						</div>
-						<div className="flex flex-row p-8 gap-x-5">
+				<div className="flex flex-row items-center">
+					<button className=''>more</button>
+					<AiOutlineArrowRight className='w-full ' />
+				</div>
+			</div>
+			<div className="p-4 bg-blue-100 rounded-lg">
+				<div className="flex flex-col pt-8 rounded-md ">
+					<div className="flex content-center px-4">
+						<Image src={monitor} className=" w-full rounded-md  transition-opacity opacity-0   duration-[1s] px-4 mb-4"
+							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='monitor' />
+					</div>
+					<div className="px-10 my-8">
+						<div className="flex flex-row">
 							<div className="px-4 basis-1/3">
 								<h3 className="text-4xl font-medium ">60<span>”</span> Monitors</h3>
 								<h2 className="my-6 text-2xl font-bold text-orange-500 ">$12,285.75</h2>
-								<button className='w-full px-4 py-3 text-base font-bold text-white uppercase bg-blue-500 rounded-md '>Add to Cart</button>
+								<button className='w-3/4 px-8 py-4 text-lg font-bold text-white bg-blue-500 rounded '>Add To Cart</button>
 							</div>
 							<div className="p-2 basis-2/3">
-								<p className="text-base font-medium">
+								<p className="text-base font-light">
 									The wide monitor offers an immersive viewing experience with its expansive display, allowing users to multitask efficiently and enjoy a panoramic view of their content. With its sleek design and crisp image quality, the 3-screen monitor enhances productivity and entertainment, providing ample screen real estate for professionals, gamers, and multimedia enthusiasts alike allowing users to multitask efficiently and enjoy a panoramic view of their content.
 								</p>
 							</div>
 						</div>
 					</div>
 				</div>
-			</section>
-		</div>
+			</div>
+		</section>
 	)
 }
 
