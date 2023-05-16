d62b29bfbb2f33bf6852a6e279e3afd7883a3984
RAKIBUL <rakibulmd933@gmail.com>
2023-05-16 20:58:56 +0600

    [ADD] products page and button

--- components/Products/page.js
+++ components/Products/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { AiOutlineArrowRight } from "react-icons/ai";
+import { BsArrowRight } from "react-icons/bs";
 import React from 'react'
 import Image from "next/image";
 import drone from "../assets/drone.jpg";
@@ -10,67 +10,23 @@ function Products() {
 			<div className='py-20'>
 				<div className="flex items-center justify-between ">
 					<div className="flex items-center ">
-						<div className="w-3 mr-4 bg-blue-700 h-11">
+						<div className="w-3 mr-4 bg-blue-400 h-11">
 						</div>
 						<h2 className="text-5xl font-bold ">Products</h2>
 					</div>
-					<div className="">
-						<button className='flex items-center '>Details <AiOutlineArrowRight className='w-12 mr-2 ' /> </button>
+					<div className="flex items-center">
+						<button className='flex items-center text-lg'>Details</button>
+						<BsArrowRight className='w-full text-lg' />
 					</div>
 				</div>
 				<div className="grid grid-cols-3 gap-16 pt-20">
-					<div className="flex flex-col justify-center">
-						<Image src={drone} className=" w-full rounded-b-md rounded-t-2xl transition-opacity opacity-0 duration-[1s] mb-8"
+					<div className="flex flex-col justify-center rounded">
+						<Image src={drone} className=" w-full rounded transition-opacity opacity-0 duration-[1s] mb-8"
 							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='drone' />
 						<div className="flex flex-col items-center">
 							<h3 className="mb-5 text-xl font-medium ">Drone</h3>
 							<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
-							<button className="w-full px-12 py-4 font-bold text-white uppercase bg-blue-400 rounded-b-2xl rounded-t-md">Add to Card</button>
-						</div>
-					</div>
-					<div className="flex flex-col justify-center ">
-						<Image src={drone} className=" w-full rounded-b-md rounded-t-2xl transition-opacity opacity-0 duration-[1s] mb-8"
-							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='drone' />
-						<div className="flex flex-col items-center">
-							<h3 className="mb-5 text-xl font-medium ">Drone</h3>
-							<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
-							<button className="w-full px-12 py-4 font-bold text-white uppercase bg-blue-400 rounded-b-2xl rounded-t-md">Add to Card</button>
-						</div>
-					</div>
-					<div className="flex flex-col justify-center ">
-						<Image src={drone} className=" w-full rounded-b-md rounded-t-2xl transition-opacity opacity-0 duration-[1s] mb-8"
-							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='drone' />
-						<div className="flex flex-col items-center">
-							<h3 className="mb-5 text-xl font-medium ">Drone</h3>
-							<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
-							<button className="w-full px-12 py-4 font-bold text-white uppercase bg-blue-400 rounded-b-2xl rounded-t-md">Add to Card</button>
-						</div>
-					</div>
-					<div className="flex flex-col justify-center ">
-						<Image src={drone} className=" w-full rounded-b-md rounded-t-2xl transition-opacity opacity-0 duration-[1s] mb-8"
-							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='drone' />
-						<div className="flex flex-col items-center">
-							<h3 className="mb-5 text-xl font-medium ">Drone</h3>
-							<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
-							<button className="w-full px-12 py-4 font-bold text-white uppercase bg-blue-400 rounded-b-2xl rounded-t-md">Add to Card</button>
-						</div>
-					</div>
-					<div className="flex flex-col justify-center ">
-						<Image src={drone} className=" w-full rounded-b-md rounded-t-2xl transition-opacity opacity-0 duration-[1s] mb-8"
-							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='drone' />
-						<div className="flex flex-col items-center">
-							<h3 className="mb-5 text-xl font-medium ">Drone</h3>
-							<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
-							<button className="w-full px-12 py-4 font-bold text-white uppercase bg-blue-400 rounded-b-2xl rounded-t-md">Add to Card</button>
-						</div>
-					</div>
-					<div className="flex flex-col justify-center ">
-						<Image src={drone} className=" w-full rounded-b-md rounded-t-2xl transition-opacity opacity-0 duration-[1s] mb-8"
-							onLoadingComplete={(image) => image.classList.remove("opacity-0")} alt='drone' />
-						<div className="flex flex-col items-center">
-							<h3 className="mb-5 text-xl font-medium ">Drone</h3>
-							<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
-							<button className="w-full px-12 py-4 font-bold text-white uppercase bg-blue-400 rounded-b-2xl rounded-t-md">Add to Card</button>
+							<button className="w-full px-12 py-4 text-lg font-bold text-white bg-blue-400 rounded">Add To Cart</button>
 						</div>
 					</div>
 				</div>
