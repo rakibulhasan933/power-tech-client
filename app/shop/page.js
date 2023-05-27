6df676fe9044a3c1b1ca58ed067e9a9fbe4e3566
RAKIBUL <rakibulmd933@gmail.com>
2023-05-27 21:54:06 +0600

    [ADD] step added

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,6 +1,10 @@
+"use client";
 import React from 'react'
 import ShopNavbar from './ShopNavbar'
-import { BiCategoryAlt } from 'react-icons/bi'
+import { BiCategoryAlt } from 'react-icons/bi';
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdPayment } from "react-icons/md";
+import { BsBookmark } from "react-icons/bs";
 
 function ShopCategories() {
 	return (
@@ -31,6 +35,26 @@ function ShopCategories() {
 						<button className="px-2 text-xl font-bold text-blue-600 bg-white border border-blue-400 cursor-pointer hover:border-2 py-7 rounded-xl">
 							Battery
 						</button>
+					</div>
+					<div className='py-20'>
+						<div className="flex justify-between ">
+							<div className="flex items-center ">
+								<BiCategoryAlt className='mr-2 text-3xl' />
+								<h3>Categories</h3>
+							</div>
+							<div className="flex items-center ">
+								<TbTruckDelivery className='mr-2 text-3xl' />
+								<h3>Shipping</h3>
+							</div>
+							<div className="flex items-center ">
+								<MdPayment className='mr-2 text-3xl' />
+								<h3>Payment</h3>
+							</div>
+							<div className="flex items-center ">
+								<BsBookmark className='mr-2 text-3xl' />
+								<h3>Saved items</h3>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
