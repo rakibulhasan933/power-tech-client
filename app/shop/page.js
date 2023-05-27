857127ee99650d8516990ffcc3b25df285ed3902
RAKIBUL <rakibulmd933@gmail.com>
2023-05-27 16:51:34 +0600

    [ADD] categories added

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ShopNavbar from './ShopNavbar'
+import { BiCategoryAlt } from 'react-icons/bi'
 
 function ShopCategories() {
 	return (
@@ -7,7 +8,10 @@ function ShopCategories() {
 			<div className="mx-40 ">
 				<ShopNavbar />
 				<div className="py-4">
-					Hello Categories
+					<div className="flex flex-row items-center py-2">
+						<BiCategoryAlt className='text-5xl font-bold' />
+						<h2 className='ml-3 text-5xl font-bold ' >Categories</h2>
+					</div>
 				</div>
 			</div>
 		</section>
