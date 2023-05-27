c71989f1163db1225898512ed5f6c2088518d03d
RAKIBUL <rakibulmd933@gmail.com>
2023-05-27 21:47:34 +0600

    [ADD] categours

--- app/shop/page.js
+++ app/shop/page.js
@@ -12,6 +12,26 @@ function ShopCategories() {
 						<BiCategoryAlt className='text-5xl font-bold' />
 						<h2 className='ml-3 text-5xl font-bold ' >Categories</h2>
 					</div>
+					<div className="grid grid-cols-1 gap-12 lg:grid-cols-3 my-14">
+						<button className="px-2 text-xl font-bold text-blue-600 bg-white border border-blue-400 cursor-pointer hover:border-2 py-7 rounded-xl">
+							Battery
+						</button>
+						<button className="px-2 text-xl font-bold text-blue-600 bg-white border border-blue-400 cursor-pointer hover:border-2 py-7 rounded-xl">
+							Battery
+						</button>
+						<button className="px-2 text-xl font-bold text-blue-600 bg-white border border-blue-400 cursor-pointer hover:border-2 py-7 rounded-xl">
+							Battery
+						</button>
+						<button className="px-2 text-xl font-bold text-blue-600 bg-white border border-blue-400 cursor-pointer hover:border-2 py-7 rounded-xl">
+							Battery
+						</button>
+						<button className="px-2 text-xl font-bold text-blue-600 bg-white border border-blue-400 cursor-pointer hover:border-2 py-7 rounded-xl">
+							Battery
+						</button>
+						<button className="px-2 text-xl font-bold text-blue-600 bg-white border border-blue-400 cursor-pointer hover:border-2 py-7 rounded-xl">
+							Battery
+						</button>
+					</div>
 				</div>
 			</div>
 		</section>
