429e1aeea86530f09ee834fe3593a1a39ae01ce4
RAKIBUL <rakibulmd933@gmail.com>
2023-05-27 16:34:00 +0600

    [ADD] shop page

---
+++ app/shop/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ShopNavbar from './ShopNavbar'
+
+function ShopCategories() {
+	return (
+		<section className=' bg-slate-50'>
+			<div className="mx-40 ">
+				<ShopNavbar />
+				<div className="py-4">
+					Hello Categories
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default ShopCategories
