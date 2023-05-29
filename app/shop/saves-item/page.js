efa683289120a98cf7d3908c57591102c172d4da
RAKIBUL <rakibulmd933@gmail.com>
2023-05-29 15:37:56 +0600

    [ADD] products added

--- app/shop/saves-item/page.js
+++ app/shop/saves-item/page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BsBookmark, BsFillStarFill } from 'react-icons/bs';
 import Image from 'next/image';
 import mac from "../../../components/assets/macbook.jpg";
+import drone from "../../../components/assets/drone.jpg";
 
 function SavesItem() {
 	return (
@@ -12,7 +13,7 @@ function SavesItem() {
 					<h2 className='ml-3 text-5xl font-bold'>Save Items</h2>
 				</div>
 			</div>
-			<div className="grid grid-cols-2 gap-1">
+			<div className="grid grid-cols-2 gap-1 my-10">
 				<div className="relative flex flex-row items-center bg-blue-100 rounded">
 					<Image src={mac} className='object-cover w-1/2 h-full rounded-md ' alt='laptop' />
 					<div className="absolute top-0bg-white left-4 w-11 h-11">
@@ -30,6 +31,86 @@ function SavesItem() {
 						</p>
 						<h2 className="my-1 text-xl font-bold text-orange-500">$400</h2>
 						<button className='w-full px-4 py-2 text-lg font-bold text-white bg-blue-400 rounded'>Add To Cart</button>
+					</div>
+				</div>
+			</div>
+			<div className="grid grid-cols-3 gap-16 my-10">
+				<div className="flex flex-col justify-center rounded">
+					<Image src={drone} className="object-cover w-full mb-8 rounded "
+						alt='drone' />
+					<div className="flex flex-col items-center">
+						<h3 className="mb-5 text-xl font-medium ">Drone</h3>
+						<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
+						<div className="flex flex-row items-center gap-0 mb-5">
+							<BsFillStarFill className='text-xs text-amber-600' />
+							<p className="ml-1 text-xs">(4.9)</p>
+						</div>
+						<button className="w-full px-12 py-4 text-lg font-bold text-white bg-blue-400 rounded">Add To Cart</button>
+					</div>
+				</div>
+				<div className="flex flex-col justify-center rounded">
+					<Image src={drone} className="object-cover w-full mb-8 rounded "
+						alt='drone' />
+					<div className="flex flex-col items-center">
+						<h3 className="mb-5 text-xl font-medium ">Drone</h3>
+						<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
+						<div className="flex flex-row items-center gap-0 mb-5">
+							<BsFillStarFill className='text-xs text-amber-600' />
+							<p className="ml-1 text-xs">(4.9)</p>
+						</div>
+						<button className="w-full px-12 py-4 text-lg font-bold text-white bg-blue-400 rounded">Add To Cart</button>
+					</div>
+				</div>
+				<div className="flex flex-col justify-center rounded">
+					<Image src={drone} className="object-cover w-full mb-8 rounded "
+						alt='drone' />
+					<div className="flex flex-col items-center">
+						<h3 className="mb-5 text-xl font-medium ">Drone</h3>
+						<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
+						<div className="flex flex-row items-center gap-0 mb-5">
+							<BsFillStarFill className='text-xs text-amber-600' />
+							<p className="ml-1 text-xs">(4.9)</p>
+						</div>
+						<button className="w-full px-12 py-4 text-lg font-bold text-white bg-blue-400 rounded">Add To Cart</button>
+					</div>
+				</div>
+				<div className="flex flex-col justify-center rounded">
+					<Image src={drone} className="object-cover w-full mb-8 rounded "
+						alt='drone' />
+					<div className="flex flex-col items-center">
+						<h3 className="mb-5 text-xl font-medium ">Drone</h3>
+						<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
+						<div className="flex flex-row items-center gap-0 mb-5">
+							<BsFillStarFill className='text-xs text-amber-600' />
+							<p className="ml-1 text-xs">(4.9)</p>
+						</div>
+						<button className="w-full px-12 py-4 text-lg font-bold text-white bg-blue-400 rounded">Add To Cart</button>
+					</div>
+				</div>
+				<div className="flex flex-col justify-center rounded">
+					<Image src={drone} className="object-cover w-full mb-8 rounded "
+						alt='drone' />
+					<div className="flex flex-col items-center">
+						<h3 className="mb-5 text-xl font-medium ">Drone</h3>
+						<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
+						<div className="flex flex-row items-center gap-0 mb-5">
+							<BsFillStarFill className='text-xs text-amber-600' />
+							<p className="ml-1 text-xs">(4.9)</p>
+						</div>
+						<button className="w-full px-12 py-4 text-lg font-bold text-white bg-blue-400 rounded">Add To Cart</button>
+					</div>
+				</div>
+				<div className="flex flex-col justify-center rounded">
+					<Image src={drone} className="object-cover w-full mb-8 rounded "
+						alt='drone' />
+					<div className="flex flex-col items-center">
+						<h3 className="mb-5 text-xl font-medium ">Drone</h3>
+						<h2 className="mb-5 text-3xl font-semibold text-orange-500 ">$305</h2>
+						<div className="flex flex-row items-center gap-0 mb-5">
+							<BsFillStarFill className='text-xs text-amber-600' />
+							<p className="ml-1 text-xs">(4.9)</p>
+						</div>
+						<button className="w-full px-12 py-4 text-lg font-bold text-white bg-blue-400 rounded">Add To Cart</button>
 					</div>
 				</div>
 			</div>
