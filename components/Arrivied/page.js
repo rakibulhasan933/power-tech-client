b9e8edff5c8211b0367f0605765e04a059c03789
RAKIBUL <rakibulmd933@gmail.com>
2023-05-29 15:22:55 +0600

    [ADD] home page card

--- components/Arrivied/page.js
+++ components/Arrivied/page.js
@@ -35,6 +35,54 @@ function Arrived() {
 							<button className='w-full px-4 py-2 text-lg font-bold text-white bg-blue-400 rounded'>Add To Cart</button>
 						</div>
 					</div>
+					<div className="flex flex-row items-center bg-blue-100 rounded">
+						<Image src={mac} className='object-cover w-1/2 h-full rounded-md ' alt='laptop' />
+						<div className="flex flex-col justify-start px-4 py-2 ">
+							<div className="flex flex-row items-center gap-0">
+								<BsFillStarFill className='text-xs text-amber-600' />
+								<p className="ml-2 text-xs">(4.9)</p>
+							</div>
+							<p className="my-1 text-xs font-light">Available now</p>
+							<h2 className="my-1 text-xl font-bold">The New POWERTECH Laptop</h2>
+							<p className="my-1 text-xs font-light">See Rich, ibrant colors and incredible
+								contrast, even in well-lit rooms.
+							</p>
+							<h2 className="my-1 text-xl font-bold text-orange-500">$400</h2>
+							<button className='w-full px-4 py-2 text-lg font-bold text-white bg-blue-400 rounded'>Add To Cart</button>
+						</div>
+					</div>
+					<div className="flex flex-row items-center bg-blue-100 rounded">
+						<Image src={mac} className='object-cover w-1/2 h-full rounded-md ' alt='laptop' />
+						<div className="flex flex-col justify-start px-4 py-2 ">
+							<div className="flex flex-row items-center gap-0">
+								<BsFillStarFill className='text-xs text-amber-600' />
+								<p className="ml-2 text-xs">(4.9)</p>
+							</div>
+							<p className="my-1 text-xs font-light">Available now</p>
+							<h2 className="my-1 text-xl font-bold">The New POWERTECH Laptop</h2>
+							<p className="my-1 text-xs font-light">See Rich, ibrant colors and incredible
+								contrast, even in well-lit rooms.
+							</p>
+							<h2 className="my-1 text-xl font-bold text-orange-500">$400</h2>
+							<button className='w-full px-4 py-2 text-lg font-bold text-white bg-blue-400 rounded'>Add To Cart</button>
+						</div>
+					</div>
+					<div className="flex flex-row items-center bg-blue-100 rounded">
+						<Image src={mac} className='object-cover w-1/2 h-full rounded-md ' alt='laptop' />
+						<div className="flex flex-col justify-start px-4 py-2 ">
+							<div className="flex flex-row items-center gap-0">
+								<BsFillStarFill className='text-xs text-amber-600' />
+								<p className="ml-2 text-xs">(4.9)</p>
+							</div>
+							<p className="my-1 text-xs font-light">Available now</p>
+							<h2 className="my-1 text-xl font-bold">The New POWERTECH Laptop</h2>
+							<p className="my-1 text-xs font-light">See Rich, ibrant colors and incredible
+								contrast, even in well-lit rooms.
+							</p>
+							<h2 className="my-1 text-xl font-bold text-orange-500">$400</h2>
+							<button className='w-full px-4 py-2 text-lg font-bold text-white bg-blue-400 rounded'>Add To Cart</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
