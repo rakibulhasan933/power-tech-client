1dbaae97ef3d412cb2edef9ef9cca9640be97c25
RAKIBUL <rakibulmd933@gmail.com>
2023-05-29 13:55:53 +0600

    [ADD] order total

--- app/shop/payment/page.js
+++ app/shop/payment/page.js
@@ -18,6 +18,43 @@ function Payment() {
 				<h2 className="my-6 text-2xl font-bold text-blue-700 ">Payment By Mobile Banking</h2>
 				<Image src={paymentImg} alt='cart' />
 			</div>
+			<div className="my-10">
+				<h2 className="mb-6 text-2xl font-bold text-blue-700">Accepted Payment Methods</h2>
+				<ul className="flex flex-col px-10 list-disc">
+					<li className="my-6 text-lg font-normal">
+						Credit Cards: We accept Visa, Mastercard, and American Express.
+					</li>
+					<li className="my-6 text-lg font-normal">
+						Debit Cards: We accept debit cards with Visa or Mastercard logo.
+					</li>
+					<li className="my-6 text-lg font-normal">
+						PayPal: Use your PayPal account for a secure and convenient payment.
+					</li>
+					<li className="my-6 text-lg font-normal">
+						Apple Pay: Pay with ease using Apple Pay on supported devices.
+					</li>
+					<li className="my-6 text-lg font-normal">
+						Google Pay: Quick and seamless payment with Google Pay.
+					</li>
+				</ul>
+			</div>
+			<div className="my-10">
+				<h2 className="mb-6 text-2xl font-bold text-blue-700">Order Total</h2>
+				<ul className="flex flex-col px-10 list-disc">
+					<li className="my-6 text-lg font-normal">
+						Shipping Fee: Additional charge for shipping, if applicable.
+					</li>
+					<li className="my-6 text-lg font-normal">
+						Subtotal: The total cost of items in your order.
+					</li>
+					<li className="my-6 text-lg font-normal">
+						Order Total: The final amount to be charged, including all fees and taxes.
+					</li>
+					<li className="my-6 text-lg font-normal">
+						Taxes: Taxes calculated based on your location and applicable laws.
+					</li>
+				</ul>
+			</div>
 		</div>
 	)
 }
