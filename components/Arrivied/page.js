1e895502e86b04fafdb297a1066889fe2d3eac42
RAKIBUL <rakibulmd933@gmail.com>
2023-05-29 20:52:58 +0600

    [ADD] add to cart

--- components/Arrivied/page.js
+++ components/Arrivied/page.js
@@ -47,7 +47,7 @@ function Arrived() {
 							<p className="my-1 text-xs font-light">See Rich, ibrant colors and incredible
 								contrast, even in well-lit rooms.
 							</p>
-							<h2 className="my-1 text-xl font-bold text-orange-500">$400</h2>
+							<h2 className="my-1 text-xl font-bold text-orange-500 lg:mb-8">$400</h2>
 							<button className='w-full px-4 py-2 text-lg font-bold text-white bg-blue-400 rounded'>Add To Cart</button>
 						</div>
 					</div>
@@ -63,7 +63,7 @@ function Arrived() {
 							<p className="my-1 text-xs font-light">See Rich, ibrant colors and incredible
 								contrast, even in well-lit rooms.
 							</p>
-							<h2 className="my-1 text-xl font-bold text-orange-500">$400</h2>
+							<h2 className="my-1 text-xl font-bold text-orange-500 lg:mb-8">$400</h2>
 							<button className='w-full px-4 py-2 text-lg font-bold text-white bg-blue-400 rounded'>Add To Cart</button>
 						</div>
 					</div>
@@ -79,7 +79,7 @@ function Arrived() {
 							<p className="my-1 text-xs font-light">See Rich, ibrant colors and incredible
 								contrast, even in well-lit rooms.
 							</p>
-							<h2 className="my-1 text-xl font-bold text-orange-500">$400</h2>
+							<h2 className="my-1 text-xl font-bold text-orange-500 lg:mb-8">$400</h2>
 							<button className='w-full px-4 py-2 text-lg font-bold text-white bg-blue-400 rounded'>Add To Cart</button>
 						</div>
 					</div>
