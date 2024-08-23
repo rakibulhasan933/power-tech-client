b45097417e566b439589499f4c1431ab24580171
MD RAKIBUL HASAN <76769537+rakibulhasan933@users.noreply.github.com>
2024-08-23 23:26:14 +0600

    Update layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google' 
 import Footer from '@/components/Footer'
 import NextAuthProvider from '@/components/NextAuthProvider'
 
